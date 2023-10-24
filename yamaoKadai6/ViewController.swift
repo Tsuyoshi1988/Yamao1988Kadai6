ad441b9711da0edf557dd94e739fad83ceda77c8
akio0911 <akio0911@gmail.com>
2023-10-24 19:10:33 +0900

    feedback

--- yamaoKadai6/ViewController.swift
+++ yamaoKadai6/ViewController.swift
@@ -10,36 +10,31 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         random()
     }
-    
 
-    
     @IBAction func hantei(_ sender: Any) {
+        let message: String
         if hanteiLabel.text == "\(Int(slider.value))" {
-            let title = "結果"
-            let message = "正解！あなたの値は\(Int(slider.value))"
-            let alert = UIAlertController(title: title, message: message,preferredStyle: .alert)
-            // OKボタンを作成＆追加
-            let ok = UIAlertAction(title: "再挑戦", style: .default, handler:{
-                action in self.random()
-            })
-            alert.addAction(ok)
-            // アラートの表示
-            present(alert, animated: true, completion: nil)
+            message = "正解！あなたの値は\(Int(slider.value))"
         } else {
-            let title = "結果"
-            let message = "ハズレ！あなたの値は\(Int(slider.value))"
-            let alert = UIAlertController(title: title, message: message,preferredStyle: .alert)
-            // OKボタンを作成＆追加
-            let ok = UIAlertAction(title: "再挑戦", style: .default, handler:{
-                action in self.random()
-            })
-            alert.addAction(ok)
-            // アラートを表示
-            present(alert, animated: true, completion: nil)
+            message = "ハズレ！あなたの値は\(Int(slider.value))"
         }
+        presentAlert(message: message)
     }
+
     func random () {
-            let index:Int = Int(arc4random_uniform(100) + 1)
-            hanteiLabel.text = "\(index)"
+        let index:Int = Int(arc4random_uniform(100) + 1)
+        hanteiLabel.text = "\(index)"
+    }
+
+    private func presentAlert(message: String) {
+        let title = "結果"
+        let alert = UIAlertController(title: title, message: message,preferredStyle: .alert)
+        // OKボタンを作成＆追加
+        let ok = UIAlertAction(title: "再挑戦", style: .default, handler:{
+            action in self.random()
+        })
+        alert.addAction(ok)
+        // アラートを表示
+        present(alert, animated: true, completion: nil)
     }
 }
